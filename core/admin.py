0906b689f5c6103f2d8b6551ac28d2028d46b60f
Remo0n <remoon.peter@gmail.com>
2021-06-19 20:44:43 +0200

    creating remote repo for django dashboard

---
+++ core/admin.py
@@ -0,0 +1,53 @@
+from django.contrib import admin
+from django.contrib.auth.models import User, Group
+
+from core.models import Device
+from rangefilter.filters import DateRangeFilter
+
+
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'entry_date', 'delivery_entity', 'delivery_engineer', 'type', 'serial_number', '_is_custody'
+                    , 'receiver_engineer', 'fixing_engineer', 'action', 'maintenance_date', 'maintenance_state'
+                    , 'receiver_name', 'sender_name', 'sending_date', 'register_date', 'ratification_doc'
+                    , 'ratification_date', 'register_number', 'exiting_date_1', 'responsible_person_1', 'exiting_date_2'
+                    , 'responsible_person_2', 'exiting_date_3', 'responsible_person_3', 'exiting_date_4'
+                    , 'responsible_person_4')
+    search_fields = ('serial_number',)
+    list_filter = ('_is_custody', 'delivery_entity', ('entry_date', DateRangeFilter),)
+    fieldsets = (
+        ('بيانات الدخول', {
+            'fields': (
+                'entry_date', 'serial_number', 'type', 'receiver_engineer', 'delivery_engineer', 'delivery_entity',
+                'fixing_engineer', 'action',),
+            'classes': ('order-0', 'baton-tabs-init', 'baton-tab-fs-register', 'baton-tab-fs-deliver'
+                        , 'baton-tab-fs-custody',),
+            'description': 'This is a description text'
+
+        }),
+        ('تسجيل الحالة الفنية', {
+            'fields': ('maintenance_date', 'maintenance_state',),
+            'classes': ('tab-fs-register',),
+            'description': 'This is another description text'
+
+        }),
+        ('بيانات التسليم', {
+            'fields': ('receiver_name', 'sender_name', 'sending_date'),
+            'classes': ('tab-fs-deliver',),
+            'description': 'This is another description text'
+        }),
+        ('فى حالة العهدة', {
+            'fields': ('register_date', 'register_person', 'ratification_doc', 'ratification_date', 'register_number'
+                       , 'exiting_date_1', 'responsible_person_1', 'exiting_date_2', 'responsible_person_2'
+                       , 'exiting_date_3', 'responsible_person_3', 'exiting_date_4', 'responsible_person_4'),
+            'classes': ('tab-fs-custody',),
+            'description': 'This is another description text'
+        }),
+    )
+
+
+# Register your models here.
+admin.site.register(Device, DeviceAdmin)
+
+# Unregister Device Model
+admin.site.unregister(User)
+admin.site.unregister(Group)
---
+++ core/models.py
@@ -0,0 +1,54 @@
+import datetime
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+# Create your models here.
+class Device(models.Model):
+    id = models.AutoField(primary_key=True, help_text=_("Id"), verbose_name=_("مسلسل"))
+    entry_date = models.DateField(default=datetime.date.today, verbose_name=_("تاريخ الدخول"))
+    delivery_entity = models.CharField(max_length=50, choices=[('عمليات', 'عمليات'), ('تدريب', 'تدريب')],
+                                       verbose_name=_("شعبة/فرع"))
+    delivery_engineer = models.CharField(max_length=50, verbose_name=_("المندوب"))
+    type = models.CharField(max_length=50, verbose_name=_("اسم الصنف"))
+    serial_number = models.CharField(max_length=50, help_text=_("Serial Number"), verbose_name=_("سريال"))
+    _is_custody = models.BooleanField(default=False, verbose_name=_("عهدة"))
+    receiver_engineer = models.CharField(max_length=50, verbose_name=_("مهندس الاستقبال"))
+    fixing_engineer = models.CharField(max_length=50, verbose_name=_("مهندس الصيانة"))
+    action = models.CharField(max_length=250, verbose_name=_("الاجراء المتخذ"))
+    maintenance_date = models.DateField(null=True, blank=True, verbose_name=_("تاريخ الاصلاح"))
+    maintenance_state = models.CharField(max_length=50, choices=[(' تم الأصلاح من الفرع', 'تم الأصلاح من الفرع'),
+                                                                 (' تم الأصلاح من الورشة', 'تم الأصلاح من الورشة'),
+                                                                 (' تم التكهين من الورشة', 'تم التكهين من الورشة'),
+                                                                 ('عاطل', 'عاطل')], verbose_name=_("تمام الأصلاح"),
+                                         blank=True)
+    receiver_name = models.CharField(max_length=50, verbose_name=_("أسم المندوب"), blank=True)
+    sender_name = models.CharField(max_length=50, verbose_name=_("القائم بالتسليم"), blank=True)
+    sending_date = models.DateField(verbose_name=_("تاريخ التسليم"), null=True, blank=True)
+    register_date = models.DateField(verbose_name=_("تاريخ تسجيل العهدة"), blank=True, null=True)
+    register_person = models.CharField(max_length=50, verbose_name=_("القائم بالتسجيل"), blank=True)
+    ratification_doc = models.CharField(max_length=50, choices=[('صح', 'صح'), ('خطأ', 'خطأ')],
+                                        verbose_name=_("طباعة طلب التصديق"), blank=True)
+    ratification_date = models.DateField(verbose_name=_("تاريخ التصديق"), blank=True, null=True)
+    register_number = models.CharField(max_length=50, verbose_name=_("رقم التصديق"), blank=True)
+    exiting_date_1 = models.DateField(verbose_name=_("تاريخ الخروج إلى الورشة"), blank=True, null=True)
+    responsible_person_1 = models.CharField(max_length=50, verbose_name=_("القائم بالمأمورية"), blank=True)
+    exiting_date_2 = models.DateField(verbose_name=_("تاريخ الاستلام من الورشة"), blank=True, null=True)
+    responsible_person_2 = models.CharField(max_length=50, verbose_name=_("القائم بالمأمورية"), blank=True)
+    exiting_date_3 = models.DateField(verbose_name=_("تاريخ الرجوع من الورشة"), blank=True, null=True)
+    responsible_person_3 = models.CharField(max_length=50, verbose_name=_("القائم بالمأمورية"), blank=True)
+    exiting_date_4 = models.DateField(verbose_name=_("تاريخ الدخول إلى الفرع"), blank=True, null=True)
+    responsible_person_4 = models.CharField(max_length=50, verbose_name=_("القائم بالمأمورية"), blank=True)
+
+    class Meta:
+        verbose_name = _("Device")
+        verbose_name_plural = _("Devices")
+
+    def save(self, *args, **kwargs):
+        custody_serial_num = ['1a', '2b']
+        if self.serial_number in custody_serial_num:
+            self._is_custody = True
+        else:
+            self._is_custody = False
+        super(Device, self).save(*args, **kwargs)
